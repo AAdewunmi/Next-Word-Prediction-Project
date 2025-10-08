b936b7993287a9d96947d4cdb037435eb5ad1b3f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-10-08 16:32:22 +0100

    Implement class PredictionLogAdmin(admin.ModelAdmin)

--- nextword_site/predictor/admin.py
+++ nextword_site/predictor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import PredictionLog
 # Register your models here.
+from django.contrib import admin
+from .models import PredictionLog
+
+
+@admin.register(PredictionLog)
+class PredictionLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "success", "seq_length_used", "latency_ms", "created_at")
+    list_filter = ("success", "created_at")
+    search_fields = ("input_text", "generated_text", "error_message")
+    readonly_fields = ("created_at",)
