73ff1a0f11883403f46c8746cc99603b28c3a006
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-10-08 15:34:46 +0100

    Refactor wsgi.py

--- nextword_site/nextword_site/wsgi.py
+++ nextword_site/nextword_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Next_Word_Prediction_Analysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nextword_site.settings')
 
 application = get_wsgi_application()
